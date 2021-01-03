60c61828bf9a4a1dfcadbf7d1c28a5c250907470
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2021-01-03 17:52:58 +0530

    Update Excersice1_Q-F_c.c

--- Chapter 3/Excersice1_Q-F_c.c
+++ Chapter 3/Excersice1_Q-F_c.c
@@ -1,12 +1,12 @@
 //Print the answer of the series (1/1! + 2/2! +3/3!...) using for loop.//
 
-
+#include<stdio.n>
 void main(){
     float i,fact = 1,n;
     float ans = 0,b;
     printf("Enter the number you like: ");
     scanf("%f",&n);
-    for(i=1;i<=n+1;i++){
+    for(i=1;i<=n;i++){
         fact = fact*i;
         printf("fact = %f, i = %f\n",fact,i);
         b = i/fact;
