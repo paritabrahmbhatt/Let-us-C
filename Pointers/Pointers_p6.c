c8cc0e1bbdd1e6867c37ce23a563abe4bab649ca
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2021-06-04 10:38:12 +0530

    Add files via upload

---
+++ Pointers/Pointers_p6.c
@@ -0,0 +1,25 @@
+//Find largest and smallest elements of an array using pointers.
+
+#include <stdio.h>
+
+void minandmax(int arr[],int len,int *min,int *max)
+{
+    *min = *max = arr[0];
+    int i;
+    for(i=1;i<len;i++)
+    {
+        if(arr[i]>*max)
+            *max = arr[i];
+        if(arr[i]<*min)
+            *min = arr[i];
+    }
+}
+void main()
+{
+    int arr[]={1,22,445,67,8,9,9,43,2,43,4,6,5,7,8,5,3,3,6,8,988,5553,0};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    int min,max;
+    minandmax(arr,len,&min,&max);
+    printf("Min and max is %d and %d respectively.",min,max);
+    return 0;
+}
